540ff8669655de5734710eddce8cc57835c44a4e
Frank Pommerening <frank@pommerening-online.de>
2016-03-06 21:53:19 +0100

    change path to projects

--- Dockerfiles/project-build/build/logging-web.sh
+++ Dockerfiles/project-build/build/logging-web.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir temp
-cp -r /projects/Spartakiade2016/Samples/ /build/temp/app/
+cp -r /projects/Spartakiade2016/Samples/Logging/ /build/temp/app/
 cp /projects/Spartakiade2016/Dockerfiles/logging-web/Dockerfile /build/temp/
 cd /build/temp/
 docker build -t fpommerening/spartakiade2016:logging-web .
