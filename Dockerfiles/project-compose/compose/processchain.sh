07ef65e812d10ee17ab844c3966a4d2ce4360014
Frank Pommerening <frank@pommerening-online.de>
2016-03-18 16:51:57 +0100

    scripts for compose

--- Dockerfiles/project-compose/compose/processchain.sh
+++ Dockerfiles/project-compose/compose/processchain.sh
@@ -1,6 +1,6 @@
 mkdir processchain
 cp -u /projects/Spartakiade2016/Dockerfiles/processchain/docker-compose.yml /compose/processchain/
 cd processchain
-/usr/local/bin/docker-compose $1
+/usr/local/bin/docker-compose $*
 cd /compose/
 rm -rf processchain
